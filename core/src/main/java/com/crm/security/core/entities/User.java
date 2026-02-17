8c3c9dd6f1ecceed57de53a0e518ad5072df0614
Eagle <lanselot4267@gmail.com>
2026-02-17 15:51:26 +0300

    Создание entity и контроллера

---
+++ core/src/main/java/com/crm/security/core/entities/User.java
@@ -0,0 +1,72 @@
+package com.crm.security.core.entities;
+
+import com.crm.security.core.enums.UserRoles;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Data
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+        @SequenceGenerator(name = "user_id_seq", sequenceName = "user_id_seq", allocationSize = 1)
+        @Column(name = "user_id")
+        private Long id;
+
+        @Column(unique = true, nullable = false)
+        private String login;
+
+        @Column(name = "password_hash", nullable = false)
+        private String password;
+
+        @Column(unique = true, nullable = false)
+        private String email;
+
+        @Enumerated(EnumType.STRING)
+        @Column(nullable = false)
+        private UserRoles role;
+
+        @Column(nullable = false)
+        private String fio;
+
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+                return List.of( new SimpleGrantedAuthority(role.name()));
+        }
+
+        @Override
+        public String getUsername() {
+                return "";
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+                return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+                return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+                return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+                return true;
+        }
+}
